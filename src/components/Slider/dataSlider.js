092843a7e53de39434df7ba12e4e96ea8ee12096
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2023-05-19 14:07:36 +0600

    did some little things

--- src/components/Slider/dataSlider.js
+++ src/components/Slider/dataSlider.js
@@ -9,13 +9,13 @@ const dataSlider = [
 	},
 	{
 		id: uuidv4(),
-		subTitle: "100% natural",
+		subTitle: ["100%", "natural"],
 		title: "Fresh Smoothie & Summer Juice",
 		text: "Best selling summer juice with natural extracts.",
 	},
 	{
 		id: uuidv4(),
-		subTitle: "100% natural",
+		subTitle: ["100%", "natural"],
 		title: "Fresh Smoothie & Summer Juice",
 		text: "Best selling summer juice with natural extracts.",
 	},
